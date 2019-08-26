f4ac13455f2adbe87625728f0b30393a1ed07d58
jmrada14 <jmrada14@tutanota.com>
2019-08-26 10:24:07 -0400

    customer program is almost ready, works just good

--- main.js
+++ main.js
@@ -18,20 +18,53 @@ let connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     run();
-    bamazon()
+    bamazon();
 });
 function run() {
-    console.log('HELLO WORLD\n')
-}
+    console.log('HELLO WORLD\n');
+};
 
-row.price = ;
 
 function bamazon() {
     connection.query('select * from products',function (err,res) {
      console.log(err,res)
         if (err) throw err;
         res.forEach(res => {
-            console.log(`id: ${row.id} name: ${row.product} price: ${row.price} stockQuantity: ${row.stock_quantity}\n\n`)
-        })
-    } )
+            console.log(`id: ${row.id} name: ${row.product} price: ${row.price} stockQuantity: ${row.stock_quantity}\n\n`);
+        });
+        questions();
+    });
 }
+
+function questions() {
+    inquirer
+        .prompt([
+        {
+            message: "Please type in the product id you would like to order.",
+            type: "input",
+            name: "productId"
+        },
+        {
+            message: "how many of this item would you like to purchase",
+            type: "input",
+            name: "productQuantity"
+        };
+    ]).then(function (ans) {
+        let productId = ans.productId;
+        let productQuantity = ans.productQuantity;
+        withdrawProduct(productId, productQuantity);
+    });
+};
+
+function withdrawProduct(productId, productQuantity){
+    connection.query('select * from products', function (err,res) {
+        console.log(err,res);
+        if(err) throw err;
+        let product;
+        for(let j = 0; j < res.length; j++){
+            product = res[j];
+        };
+
+    }
+    );
+};
