fe5b61a1ab88fa8dc44223bfba83f0bf130b77a5
jmrada14 <jmrada14@tutanota.com>
2019-08-28 12:35:02 -0400

    view products and interface for manager done

---
+++ manager.js
@@ -0,0 +1,57 @@
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+
+let connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "juan",
+
+    // Your password
+    password: "juan",
+    database: "bamazonDB"
+});
+
+connection.connect();
+askQuestions()
+function askQuestions() {
+    inquirer.prompt([
+        {
+            message: "What would you like to do?",
+            type: "list",
+            name: "managerAction",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+        }
+    ]).then(function (ans) {
+        switch (ans.manager) {
+            case "Products for sale":
+                viewProducts()
+                break;
+            case "View low inventory":
+                lowInventory()
+                break;
+            case "Add product to inventory":
+                addInventory()
+                break;
+            case "Add new products to db":
+                addProduct()
+                break;
+            default:
+
+                break;
+        }
+    });
+}
+function viewProducts () {
+    connection.query('SELECT * FROM products', function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+        res.forEach(row => {
+            console.log(`id: ${row.id} Name: ${row.product} Price: ${row.price} Quantity: ${row.stock_quantity}\n`)
+        });
+        connection.end()
+    })
+}
